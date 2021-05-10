576285f63cc4f6e6a6a056d8e4ba749bfd1a0659
Stian <69841774+StianPost@users.noreply.github.com>
2021-05-10 20:15:18 +0200

    fixed my dumb mistakes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gameAPI =
-  'https://noroffcors.herokuapp.com/http://postal.one/wp-json/wc/store/products';
+  'https://noroffcors.herokuapp.com/http://postal.one/wp-json/wp/v2/posts/';
 const loading = document.querySelector('.loading');
 const FBlogs = document.querySelector('.feauturedContainer');
 
@@ -9,26 +9,28 @@ async function getGameAPI(url) {
     const result = await response.json();
     console.log(result);
     loading.innerHTML = '';
-    for (let i = 0; i < result.length; i++) {}
-    FBlogs.innerHTML = `
-    <div class="featuredCard">
-        <div class="imgBackground">
-            <div class="featuredCard__tags">
-                <div class="featuredCard__tags--tag">Fitness</div>
-                <div class="featuredCard__tags--tag">Food</div>
-                <div class="featuredCard__tags--tag">Yoga</div>
-                <div class="featuredCard__tags--tag">Weightloss</div>
+    for (let i = 0; i < result.length; i++) {
+      FBlogs.innerHTML += `
+        <div class="featuredCard">
+            <div class="imgBackground">
+                <div class="featuredCard__tags">
+                    <div class="featuredCard__tags--tag">Fitness</div>
+                    <div class="featuredCard__tags--tag">Food</div>
+                    <div class="featuredCard__tags--tag">Yoga</div>
+                    <div class="featuredCard__tags--tag">Weightloss</div>
+                </div>
+            </div>
+            <div class="featuredCard__text">
+                <h3 class="featuredCard__text--tittel">Tittel</h3>
+                <p>text</p>
+                <p><a href="/blog_page.html?id=${result[i].id}">Read More...</a></p>
             </div>
         </div>
-        <div class="featuredCard__text">
-            <h3 class="featuredCard__text--tittel">Tittel</h3>
-            <p>
-            text
-            </p>
-            <p><a href="/blog_page.html?id=${result[i].id}>Read More...</a></p>
-        </div>
-    </div>
-      `;
+          `;
+      if (i === 4) {
+        break;
+      }
+    }
   } catch (error) {
     console.log(error);
     document.querySelector('.alert').innerHTML = showAlertTouser(
