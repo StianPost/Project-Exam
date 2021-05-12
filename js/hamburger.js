88274bbbcddb33274a598f6ccb55a46cee64009e
Stian <69841774+StianPost@users.noreply.github.com>
2021-05-12 15:10:59 +0200

    tweaked mobile menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,12 +1,15 @@
 const hamburgerOpen = document.querySelector('#hamburgerOpenBtn');
 const hamburgerClose = document.querySelector('#hamburgerCloseBtn');
 const overlay = document.querySelector('#overlay');
+const logo = document.querySelector('.headerDiv__left');
 
 hamburgerOpen.onclick = function () {
   overlay.classList.remove('hide');
   hamburgerOpen.classList.add('hide');
+  logo.classList.add('hide');
 };
 hamburgerClose.onclick = function () {
   overlay.classList.add('hide');
   hamburgerOpen.classList.remove('hide');
+  logo.classList.remove('hide');
 };
